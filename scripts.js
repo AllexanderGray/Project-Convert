00a502c1c1cf2caf3f4489022bafdfe5e80aafa1
Felipe Carvalho <feboross@gmail.com>
2024-10-20 15:51:09 -0300

    Adicionado código relacionado a moedas (Preparação dos valores para conversão).

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
+//Cotação de moedas
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
 
+//Obtendo os valores do formulário
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
+// Manipulando o input para receber somente números
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
+});
+
+//Capturando o evento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+    default:
+      break;
+  }
+};
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol);
+}
